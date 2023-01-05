458e9984a6869bbe52163b1e483460305900e8e9
AURELIO MALHEIROS <aureliomalheiros@gmail.com>
2023-01-05 00:05:12 -0300

    feat: Install programs - Install basic programs - Update system - Configuration $HOME - Install plugins

--- config.sh
+++ config.sh
@@ -3,7 +3,13 @@ function updateSystem () {
     sudo apt update >> /dev/null 2>&1 && sudo apt upgrade -y > /dev/null 2>&1
 }
 function configHome () {
+    echo "[CONFIGURATION $HOME]"
     cp -RT home/ $HOME
+}
+function configVIM () {
+    echo "[CONFIGURATION VIM]"
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    vim +PluginInstall +qall
 }
 function basicPrograms () {
     sudo apt install -y \
@@ -18,40 +24,13 @@ function basicPrograms () {
         software-properties-common
         
 }
-function languageGO (){
-
-}
-function k8sKubeCtl (){
-    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-    curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
-    echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
-    sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-    chmod +x kubectl
-    mkdir -p ~/.local/bin
-    mv ./kubectl ~/.local/bin/kubectl
-}
-function ohMyZSH () {
-
-}
-function virtualization () {
-
-}
-
-function terraform () {
-    wget -O- https://apt.releases.hashicorp.com/gpg | \
-    gpg --dearmor | \
-    sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
-    gpg --no-default-keyring \
-    --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
-    --fingerprint
-    echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
-    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
-    sudo tee /etc/apt/sources.list.d/hashicorp.list
-    sudo apt update > /dev/null 2>&1
-    sudo apt install -y terraform
-}
-
+# Install Golang
+# Virtualization -> Vagrant and virtualbox
+# Install Docker
+# Kubectl
+# BasicPrograms -> FlameShot, Peek, insync
+# Configuration ZSH with theme power10
 updateSystem
 configHome
+configVIM
 basicPrograms
-
