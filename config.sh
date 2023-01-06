39c24d83fb731a87e3a9d835db4ade25ed295cd4
Aurelio Malheiros <aurelio.malheiros@unico.io>
2023-01-06 14:41:16 -0300

    fix: add -> \ in the function

--- config.sh
+++ config.sh
@@ -19,9 +19,8 @@ function basicPrograms () {
         lsb-release \
         apt-transport-https \
         flameshot \
-        peek 
-        wget https://go.dev/dl/go1.19.4.linux-amd64.tar.gz
-        rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.4.linux-amd64.tar.gz
+        peek \
+        wget https://go.dev/dl/go1.19.4.linux-amd64.tar.gz && rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.4.linux-amd64.tar.gz \
         export PATH=$PATH:/usr/local/go/bin
 }
 function configVIM () {
@@ -36,14 +35,14 @@ function k8s (){
     wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 }
 function docker (){
-    sudo apt-get remove docker docker-engine docker.io containerd runc
+    sudo apt-get remove docker docker-engine docker.io containerd runc -y
     sudo mkdir -p /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     echo \
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt update
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
     sudo chmod 666 /var/run/docker.sock
 }
 function MyZSHWithThemePowerlevel10 () {
