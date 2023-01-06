a1488b9a3ed9cb9baa914cca34086fa084213e5d
Aurelio Malheiros <aureliomalheiros@gmail.com>
2023-01-06 20:17:52 -0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -51,8 +51,8 @@ function MyZSHWithThemePowerlevel10 () {
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k 
 }
 updateSystem
-configHome
 basicPrograms
+configHome
 configVIM
 k8s
 docker
