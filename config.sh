0994495b97116a99011a0a54092a8304fb3d6932
Aurelio Malheiros <aureliomalheiros@gmail.com>
2023-01-09 18:43:21 -0300

    feat: Add install helm - install helm

--- config.sh
+++ config.sh
@@ -32,6 +32,11 @@ function k8s (){
     sudo apt update
     sudo apt install -y kubectl
     wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+    curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+    sudo apt-get install apt-transport-https --yes
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+    sudo apt-get update
+    sudo apt-get install helm -y
 }
 function docker (){
     sudo apt-get remove docker docker-engine docker.io containerd runc -y
